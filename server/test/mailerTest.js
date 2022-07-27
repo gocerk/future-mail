a850452c5ae3fbc5fedbacb35662b93fa397670a
kaan <65464708+kaanfkennedy@users.noreply.github.com>
2022-07-27 20:53:41 +0300

    Update mailerTest.js

--- server/test/mailerTest.js
+++ server/test/mailerTest.js
@@ -5,7 +5,7 @@ const { send } = require('../src/service/Mailer');
 
 
 test('sending mail test', async () => {
-    const response = send({to: 'gocerkaan2006@gmail.com', msg: 'test', 'deliveryDate': new Date('2022-12-05'), 'writingDate': new Date('2022-5-08')});
+    const response = send({to: 'XXX@gmail.com', msg: 'test', 'deliveryDate': new Date('2022-12-05'), 'writingDate': new Date('2022-5-08')});
     
     assert.strictEqual(response, true);    
 });
